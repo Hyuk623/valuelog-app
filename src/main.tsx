038de1568fb1475128362d98624bc66109de00e3
Hyuk623 <chanhyuck623@gmail.com>
2026-01-11 22:25:19 +0900

    Downgraded Vite to stable v6.0.1 to resolve build crash

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,1 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ErrorBoundary } from './components/common/ErrorBoundary'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </StrictMode>,
-)
+console.log('Hello');
